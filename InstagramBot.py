bb1ae8dea4d2f5bf1f38e2f78d8dccd6b2ba4983
hasanarcas <57601422+hasanarcas@users.noreply.github.com>
2021-03-27 18:43:10 +0300

    Update InstagramBot.py

--- InstagramBot.py
+++ InstagramBot.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from Classes import HomePage
 from time import sleep
-browser = webdriver.Firefox()           # open the firefox browser
+browser = webdriver.Firefox()                       # open the firefox browser
 browser.implicitly_wait(5)
 
-home_page = HomePage(browser)
-login_page = home_page.go_to_login_page()
-login_page.login("yddos1", "3208411505b")
+home_page = HomePage(browser)                       #creating a HomePage object that will store the home_page
+login_page = home_page.go_to_login_page()           #calling the go_to_login_page method in order to login in the appropriate page
+login_page.login("my username", "<my password")     #giving the appropriate usarname and password in order to login with my account
 
 
-sleep(50)
+sleep(50)                                           #the program will wait 50 seconds before it's closed 
 browser.close()
